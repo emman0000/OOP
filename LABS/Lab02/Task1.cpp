355854f48a5b15ba4ae96395b7879975cdc8d39d
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-11 08:54:02 +0500

    Update Task1.cpp

--- LABS/Lab02/Task1.cpp
+++ LABS/Lab02/Task1.cpp
@@ -1,1 +1,21 @@
-
+//Programmer Emman Ali-23k0051 LAB02 Task 1
+#include <iostream>  
+using namespace std;
+void RecursiveSwap(int &x, int &y, int count) {  
+    if (count == 0) {  
+        return;  
+    } else {  
+        int temp = x;  
+        x = y;   y = temp; 
+        int c = count-1; //decrement count for recursive swap
+       RecursiveSwap(x, y, c );  
+    }  
+}  
+//Main function
+int main() {  
+    int x = 7, y = 9;  
+cout << "Before swapping: x = " << x << ", y = " << y << endl;  
+RecursiveSwap(x, y, 1); //I passed three values into the function I called the last one for the cout to continually swap the numbers until it decrements to 0. 
+    cout << "After swapping: x = " << x << ", y = " << y << endl;  
+    return 0;  
+}//end main 
