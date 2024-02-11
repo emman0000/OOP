48137ee1f767c03bf9c3e586f4ca7bd51a268a4e
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-11 21:06:05 +0500

    Update Task1.cpp

--- LABS/Lab02/Task1.cpp
+++ LABS/Lab02/Task1.cpp
@@ -1,16 +1,18 @@
 //Programmer Emman Ali-23k0051 LAB02 Task 1
 #include <iostream>  
 using namespace std;
+//function for swapping
 void RecursiveSwap(int &x, int &y, int count) {  
     if (count == 0) {  
         return;  
-    } else {  
+    } //end if 
+    else {  
         int temp = x;  
         x = y;   y = temp; 
         int c = count-1; //decrement count for recursive swap
        RecursiveSwap(x, y, c );  
-    }  
-}  
+    } //end else 
+}//end function  
 //Main function
 int main() {  
     int x = 7, y = 9;  
