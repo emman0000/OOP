6a80e1bd69afcae0eddbe30ade8de04528849b37
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-11 21:04:53 +0500

    Update Task2.cpp

--- LABS/Lab02/Task2.cpp
+++ LABS/Lab02/Task2.cpp
@@ -2,23 +2,23 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+//created a structure here for book and included the variables with a range of data types.
 struct Book{
     string title; string author; int year; string genre;  
-};
-//Function to display previously added books already in the system 
+};//end struct
+//Function to display previously added books already in the system (straightforward)
 void displayBook(const Book& book){
   cout<<" Title of the Book: "<<book.title<<endl; cout<<" Author of the Book:"<<book.author<<endl; cout<<" Year of Publishing:"<<book.year<<endl; cout<<" Genre of the Book:\n"<<book.genre<<endl;
 }//end function 
 
-//function to search book either by title or by author name
+//function to search book either by title or by author name uses bool and compares the stored data itself. Could have used a strlen method too.
 void searchBook(const Book books[], int s, const string& comp){
 bool exists = false; for(int i=0; i<s; i++){
   if(books[i].title==comp || books[i].author==comp){ displayBook(books[i]); exists = true;}}//end loop
 if (!exists) {cout<<"No book found with the given title or author."<<endl;}
 }//end function
 
-//function to add a new book to the system
+//function to add a new book to the system (straightforward)
 void addABook(Book books[], int&NoOfBooks){
   int addedBooks; cout<<"Enter the number of books you want to add:"; cin>>addedBooks; cin.ignore();
 //loop for adding books 
@@ -34,11 +34,10 @@ void addABook(Book books[], int&NoOfBooks){
 void AllBooks(const Book books[], int NoOfBooks){
   cout<<"Books in the Inventory are: "<<endl; for(int i=0; i<NoOfBooks; i++){
   displayBook(books[i]);}}//end function
-
+//main function starts
 int main () {
 const int Total_Capacity = 7; //total capacity of the array allows size of array to remain fixed whilst code is running
 Book books[Total_Capacity]; int NoOfBooks = 0; //NoOfBooks when user is yet to add any books.
-
 
  books[NoOfBooks++] = {"Are You Afraid Of The Dark?", "Sidney Sheldon", 2004, "Thriller"};
  books[NoOfBooks++] = {"IT", "Stephen King", 1986, "Horror"};
