548786c653d4e0f3ccf475cafe20847f7c0d22d8
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-11 21:01:53 +0500

    Update Task3.cpp

--- LABS/Lab02/Task3.cpp
+++ LABS/Lab02/Task3.cpp
@@ -1,30 +1,28 @@
 Programmer Emman Ali-23k0051 LAB02 Task 3
-//Programmer Emman Ali-23k0051, LAB02 Task3.
 #include<iostream>
 using namespace std;
-
-bool hasSubsetSum(int arr[], int size, int targetSum) {
-    if(size == 0) {
-        return targetSum == 0;
+//bool function
+bool hasSubsetSum(int array[], int arraySize, int target) {
+    if(arraySize == 0) {
+        return target == 0;
     }
-    return hasSubsetSum(arr, size - 1, targetSum) || hasSubsetSum(arr, size - 1, targetSum - arr[size - 1]);
+//two recursive calls that exclude the last number and maintain the target sum.
+    return hasSubsetSum(array, arraySize - 1, target) || hasSubsetSum(array, arraySize - 1, target - array[arraySize - 1]);
 }
-
+//main function
 int main() {
-    int arr[] = {3, 34, 4, 12, 5, 2};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int array[] = {3, 34, 4, 12, 5, 2};
+    int size = sizeof(array) / sizeof(array[0]);
     int targetSum;
-
-    cout << "Enter the target: ";
-    cin >> targetSum;
-
-    bool result = hasSubsetSum(arr, size, targetSum);
-
+    cout << "Enter a target numbe: ";    cin >> targetSum;
+    bool result = hasSubsetSum(array, size, targetSum); //calling function 
+//Show results 
     if (result) {
         cout << "There exists a subset of the array that sums up to the target sum." << endl;
-    } else {
+    }//end if 
+    else {
         cout << "There does not exist a subset of the array that sums up to the target sum." << endl;
-    }
+    }//end else
 
     return 0;
 }//end main 
