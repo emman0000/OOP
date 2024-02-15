a4d8355122304dd5afb9cb4ccb15507126e139c7
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-15 15:04:23 +0500

    Create task1.cpp

---
+++ LABS/Lab04/task1.cpp
@@ -0,0 +1,29 @@
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book{
+  string name; string author; int ISBN; int count;  int numpages;
+  public: 
+  //constructor
+  book(){
+      name = "Pride and Prejudice"; author = "Jane Austen"; ISBN=78689; numpages= 500;
+  }
+  
+  
+ //patameterised constructor 
+};
+
+//default construct 
+//parametrised construct
+void pageread()
+
+
+
+int main() {
+   
+
+    return 0;
+}
