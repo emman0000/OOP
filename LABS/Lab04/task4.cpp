32903f0c5c34b77acab67395f3ca00b307a622cb
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-22 22:41:44 +0500

    Update task4.cpp

--- LABS/Lab04/task4.cpp
+++ LABS/Lab04/task4.cpp
@@ -1,58 +1,51 @@
-
-//Programmer Emman Abrar Ali 23k0051 Lab04 Task3
-
-#include<iostream>
+//Programmer Emman Abrar Ali 23k0051 Lab04 Task4
 #include<string>
+#include<iostream>
 using namespace std;
-//Create a class called Weekdays 
-class WeekDays {
-private:
-    string Days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; //string array because how else will you store words
-    int CurrentDay; //duh
 
+class Office {
+    string Location; 
+    int SeatingCapacity; 
+    string Furniture[3]; 
 public:
-    // Default constructor usually no need to make this since program will run one anyway
-    WeekDays() {
-        CurrentDay = 0; // Default current day is Sunday
-    }//end of default constructor 
+    // Para constructor
+    Office(string location = " ", int SeatingCap = 0, string furniture[3] = {" ", " ", " "})
+        : Location(location), SeatingCapacity(SeatingCap) { // using a member initialisation list here and default values as mentioned 
+        for(int i = 0; i < 3; i++) {
+            Furniture[i] = furniture[i];
+        }
+    } // End of constructor
 
-    // Parameterised constructor
-    WeekDays(int n) {
-        CurrentDay = n % 7; // we use %7 so that the days stay within the loop 
-    }//end of PC
-//Making a getter function for Current Day since it is a private data member and we want to be able to make  changes and use it.
-    string getCurrentDay() {
-        return Days[CurrentDay];
-    }//end of function 
-    string getNextDay() {
-        return Days[(CurrentDay + 1) % 7]; 
-    } string getPreviousDay() {
-        return Days[(CurrentDay + 6) % 7]; 
-    }
-    string getNthDayFromToday(int n) {
-        return Days[(CurrentDay + n) % 7];
-    }
-    void setCurrentDay(int n) {
-        CurrentDay = n % 7; // Ensure CurrentDay stays within the loop of 7 days otherwise they will be out of bounds
-    }
-};
+    // Function to display office details
+    void display() {
+        cout << "Location of the Office is: " << Location << endl; 
+        cout << "The Seating Capacity for the Office is: " << SeatingCapacity << endl; 
+        cout << "The Furniture: "; 
+        for(int i = 0; i < 3; i++) {
+            cout << Furniture[i] << " "; 
+        }
+        cout << endl;
+    } // End of display function
+}; // End of class
 
 int main() {
-    int CD;
-    cout << "SUN=0, MON=1, TUE=2, WED=3, THU=4, FRI=5, SAT=6" << endl;
-    cout << "Enter a number for a day of the week: ";
-    cin >> CD;
+    Office PandG("Clifton Block 3", 60, {"Chairs", "Tables", "Shelves"}); 
+    Office Unilever("Avari Towers", 90, {"Cubicle", "Desks", "Chairs"}); 
+    Office GSKPharma("Ocean Front Clifton", 70, {"Chairs", "Tables", "Boards"}); 
+    Office CocaCola; // Leave this for the default constructor 
 
-    WeekDays W(CD);
-
-    cout << "The current day is " << W.getCurrentDay() << endl;
-    cout << "The next day is " << W.getNextDay() << endl;
-    cout << "The previous day was " << W.getPreviousDay() << endl;
-
-    int n;
-    cout << "Enter a number n to find the nth day from today: ";
-    cin >> n;
-    cout << "The nth day from today will be " << W.getNthDayFromToday(n) << endl;
+    cout << "P and G: " << endl; 
+    PandG.display(); 
+    cout << endl;
+    cout << "Unilever:" << endl; 
+    Unilever.display(); 
+    cout << endl;
+    cout << "GSKPharma:" << endl; 
+    GSKPharma.display(); 
+    cout << endl;
+    cout << "The Coca Cola Company:" << endl; 
+    CocaCola.display(); 
+    cout << endl;
 
     return 0;
-}//end main 
+} // End main
