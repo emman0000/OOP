dae14c8a1dcd5193cf12345673e0519473ec44e9
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-02-22 22:56:51 +0500

    Update task4.cpp

--- LABS/Lab04/task4.cpp
+++ LABS/Lab04/task4.cpp
@@ -1,50 +1,55 @@
 //Programmer Emman Abrar Ali 23k0051 Lab04 Task4
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
+#include <initializer_list>
+
 using namespace std;
 
 class Office {
-    string Location; 
-    int SeatingCapacity; 
-    string Furniture[3]; 
+    string Location;
+    int SeatingCapacity;
+    string Furniture[3];
+
 public:
-    // Para constructor
-    Office(string location = " ", int SeatingCap = 0, string furniture[3] = {" ", " ", " "})
-        : Location(location), SeatingCapacity(SeatingCap) { // using a member initialisation list here and default values as mentioned 
-        for(int i = 0; i < 3; i++) {
-            Furniture[i] = furniture[i];
+    // Parameterized constructor
+    Office(string location = " ", int SeatingCap = 0, initializer_list<string> furniture = {" ", " ", " "})
+        : Location(location), SeatingCapacity(SeatingCap) {
+        int i = 0;
+        for (const auto& item : furniture) {
+            Furniture[i++] = item;
+            if (i >= 3) break;
         }
-    } // End of constructor
+    }
 
     // Function to display office details
     void display() {
-        cout << "Location of the Office is: " << Location << endl; 
-        cout << "The Seating Capacity for the Office is: " << SeatingCapacity << endl; 
-        cout << "The Furniture: "; 
-        for(int i = 0; i < 3; i++) {
-            cout << Furniture[i] << " "; 
+        cout << "Location of the Office is: " << Location << endl;
+        cout << "The Seating Capacity for the Office is: " << SeatingCapacity << endl;
+        cout << "The Furniture: ";
+        for (int i = 0; i < 3; i++) {
+            cout << Furniture[i] << " ";
         }
         cout << endl;
-    } // End of display function
+    }
 }; // End of class
 
 int main() {
-    Office PandG("Clifton Block 3", 60, {"Chairs", "Tables", "Shelves"}); 
-    Office Unilever("Avari Towers", 90, {"Cubicle", "Desks", "Chairs"}); 
-    Office GSKPharma("Ocean Front Clifton", 70, {"Chairs", "Tables", "Boards"}); 
+    Office PandG("Clifton Block 3", 60, {"Chairs", "Tables", "Shelves"});
+    Office Unilever("Avari Towers", 90, {"Cubicle", "Desks", "Chairs"});
+    Office GSKPharma("Ocean Front Clifton", 70, {"Chairs", "Tables", "Boards"});
     Office CocaCola; // Leave this for the default constructor 
 
-    cout << "P and G: " << endl; 
-    PandG.display(); 
+    cout << "P and G: " << endl;
+    PandG.display();
     cout << endl;
-    cout << "Unilever:" << endl; 
-    Unilever.display(); 
+    cout << "Unilever:" << endl;
+    Unilever.display();
     cout << endl;
-    cout << "GSKPharma:" << endl; 
-    GSKPharma.display(); 
+    cout << "GSKPharma:" << endl;
+    GSKPharma.display();
     cout << endl;
-    cout << "The Coca Cola Company:" << endl; 
-    CocaCola.display(); 
+    cout << "The Coca Cola Company:" << endl;
+    CocaCola.display();
     cout << endl;
 
     return 0;
