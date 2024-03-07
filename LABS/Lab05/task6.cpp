27dfb403449ab8217512857de7a60fadc5c22d89
emman0000 <142867499+emman0000@users.noreply.github.com>
2024-03-07 22:37:36 +0500

    Update task6.cpp

--- LABS/Lab05/task6.cpp
+++ LABS/Lab05/task6.cpp
@@ -1,1 +1,47 @@
+//Task 06 Lab05 23k0051 Emman Abrar Ali
+#include<iostream>
+#include<string>
+using namespace std;
 
+class GraphicsEngine{
+public:
+void renderGraphicsEngine(){
+  cout<<"Graphics Engine is rendering graphics... "<<endl;
+}//end function
+};//end the class 
+
+class InputHandler{
+public:
+void handleInput(){
+  cout<<"Input Handler is handling the input... "<<endl;
+}//end function
+};//end class Input Handler
+
+class PhysicsEngine{
+public:
+void handlePhysics(){
+cout<<"Physics Engine is handling the physics... "<<endl;/
+}//end function
+};//end of class Physics Engine
+
+class GameEngine{
+GraphicsEngine graphicwalaengine;
+InputHandler inputwalahandler;
+PhysicsEngine physicswalaengine;
+
+public:
+void startgame(){
+
+  graphicwalaengine.renderGraphicsEngine();
+  inputwalahandler.handleInput();
+  physicswalaengine.handlePhysics();
+
+}//end function
+};//end class  Engine
+
+int main (){
+cout<<"Emman Abrar Ali 23k0051"<<endl;
+GameEngine gameengine;
+  gameengine.startgame();
+  return 0;
+}//end main
