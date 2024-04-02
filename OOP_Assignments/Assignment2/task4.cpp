583473040a7e27a799fb16df73537b7d897b4fb6
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-04-02 21:47:30 +0500

    Update task4.cpp

--- OOP_Assignments/Assignment2/task4.cpp
+++ OOP_Assignments/Assignment2/task4.cpp
@@ -3,33 +3,41 @@
 #include <iostream>
 #include <string>
 using namespace std;
+static const int MAX_FEED_SIZE = 10; //making a constant size for the number of pictures per account.
 
-static const int MAX_FEED_SIZE = 10;
+class Post {
+protected:
+    string postID; string postContent; int likes; string comments;
 
-// Forward declaration for class Post
-class Post;
+public:
+    Post(const string& id, const string& pc, int l, const string& c)
+        : postID(id), postContent(pc), likes(l), comments(c) {}//end 
+
+    virtual void displayDetails() {
+        cout<<"_____________________________________"<<endl;
+        cout << "Post content: " << postContent << endl;
+        cout << "Post ID: " << postID << endl;
+        cout << "Likes: " << likes << endl;
+        cout << "Comments: " << comments << endl;
+        cout << endl;
+         cout<<"_____________________________________"<<endl;
+    }//end function 
+
+    virtual ~Post() {} // virtual destructor to ensure proper cleanup of derived classes
+};//end class
 
 class User {
 protected:
-    string username;string email; string password;
-
-public:
+    string username; string email; string password;
+ 
+ public:
     User(const string& username, const string& email, const string& password)
-        : username(username), email(email), password(password) {} //end
+        : username(username), email(email), password(password) {}
 
-    // Getters and setters
-    string getUsername() const {
-        return username;
-    }
-    void setUsername(const string& username) {
-        this->username = username;
-    }
-    bool verifyPassword(const string& inputPassword) {
-        return inputPassword == password;
-    }
-};//eend class User
+    virtual void viewFeed() const = 0; // pure virtual function, making User an abstract class
 
-// Derived classes
+    virtual ~User() {} // virtual destructor to ensure proper cleanup of derived classes
+};//end class 
 
 class RegularUser : public User {
 protected:
@@ -37,107 +45,54 @@ protected:
     int numPosts;
 
 public:
-    RegularUser(const string& username, const string& email, const string& password, int nop)
+    RegularUser(const string& username, const string& email, const string& password)
         : User(username, email, password), numPosts(0) {}
 
-    // Function to add post to feed
     void addToFeed(Post* post) {
+        cout<<"Adding Post..."<<endl;
         if (numPosts < MAX_FEED_SIZE) {
-            feed[numPosts] = post;
-            numPosts++;
-            cout << "Image added successfully\n" << endl;
+            feed[numPosts++] = post;
+            cout << "Post added successfully\n" << endl;
+            cout<<"******************************"<<endl;
         } else {
             cout << "No more space available. Upgrade storage." << endl;
-        }
-    }
+        }//end else 
+    }//end function 
 
-    // Function to view feed
-    void viewFeed();
-
-};//end class RegularUser
+    void viewFeed() const override {
+        cout << "Viewing feed for " << username << ":" << endl;
+        for (int i = 0; i < numPosts; ++i) {
+            feed[i]->displayDetails();
+        }//end for loop
+    }//end function 
+};//end class 
 
 class BusinessUser : public User {
 public:
     BusinessUser(const string& username, const string& email, const string& password)
-        : User(username, email, password) {}//end constructor
+        : User(username, email, password) {}
 
-    // Function to promote the post 
-    void promotePost(Post* post);
-    void viewFeed();
+    void promotePost(Post* post) {
+        cout << "Trending today!" << endl;
+        post->displayDetails();
+    }//end function 
 
-};//end class BusinessUser
-
-class Post {
-protected:
-    string postID;
-    string postContent;
-    int likes;
-    string comments;
-
-public:
-    Post(const string& id, const string& pc, int l, const string& c)
-        : postID(id), postContent(pc), likes(l), comments(c) {}
-
-    // Function to add comments 
-    void addComments(const string& comment) {
-        comments += comment + "\n";
-    }
-
-    // Function to like post
-    void likePost() {
-        likes++;
-        cout << "Post liked" << endl;
-    }
-
-    string getPostID() const {
-        return postID;
-    }
-
-    void displayDetails() {
-        cout << "Post content: " << postContent << endl;
-        cout<<"Image Id: "<<postID<<endl;
-        cout << "Likes: " << likes << endl;
-        cout << "Comments: " << comments << endl;
-        cout<<"\n \n";
-    }
-
-};//end class Post
-
-// Implementations using :: to gain access
-
-void RegularUser::viewFeed() {
-    cout << "Viewing feed for " << getUsername() << ":" << endl;
-    for (int i = 0; i < numPosts; ++i) {
-        feed[i]->displayDetails();
-    }
-}//end function
-
-void BusinessUser::promotePost(Post* post) {
-    cout << "Trending today!\n " << post->getPostID() << endl;
-    cout << "....................................." << endl;
-    // Logic to increase post likes and views for business user
-}//end function 
+    void viewFeed() const override {
+//empty function shows nothing just kept it there 
+    }//end function
+};//end class
 
 int main() {
-    cout << "Programmer Emman Abrar Ali 23k0051" << endl;
-    cout << "----------------------------------\n" << endl;
-
-    RegularUser regularUser("Areeba Hasnain", "Areeba1@gmail.com", "password123", 0);
+    RegularUser regularUser("Areeba Hasnain", "Areeba1@gmail.com", "password123");
     BusinessUser businessUser("Papa's Cupcakes", "Papascupcake@gmail.com", "vanillafrosting");
-    Post post1("001", "Beach day with friends ", 77, "Looking sunny!");
+    Post post1("001", "Beach day with friends", 77, "Looking sunny!");
     Post post2("002", "Our all new Lotus cream cupcake", 550, "LOOKS SO YUMMM!");
 
-//Promoting business user's content
     businessUser.promotePost(&post2);
-
-//Adding images to feed for both users 
     regularUser.addToFeed(&post1);
     regularUser.addToFeed(&post2);
-
     regularUser.viewFeed();
+    businessUser.viewFeed();
 
     return 0;
 }//end main 
-
-
-
