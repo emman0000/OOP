f989f659236c7c3b2b045af2d1646722201549b9
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-04-25 11:03:37 +0500

    Update task2.cpp

--- LABS/Lab09/task2.cpp
+++ LABS/Lab09/task2.cpp
@@ -1,1 +1,42 @@
+//Programmer Emman Ali 23k0051
+//Lab 09 Task 2
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Number{
+int number;
+public: Number(int n):number(n){}//end constructor
+int  getNumber(){return number;}//end function
+void setNumber(int n){number=n;}//end function
+
+//Overloading prefix decrement operator --
+Number operator--(){
+  number/=4; return *this; //if it is prefix then the number is x by  4
+}//end function
+
+Number operator--(int){
+  Number temp = *this;
+  number/=4; return temp;
+  //if the number is post it is / by 4
+}//end function
+
+//display
+void display(){
+  cout<<" "<<number<<endl;
+}//end 
+
+};//end class Number 
+
+int main(){
+Number n(28); 
+cout<<"OG number: "; n.display();
+--n; 
+  cout<<"Prefix After effects: ";n.display();
+n--;
+cout<<"Postfix After effects: ";n.display();
+
+
+ return 0; 
+}//end main 
