d81043f4fb7369fb51f76969c5922c7cd06df458
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-04-25 22:16:01 +0500

    Update task1.cpp

--- LABS/Lab09/task1.cpp
+++ LABS/Lab09/task1.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Logo {
+
+    string logo;
+
+public:
+    Logo(const string& logo) : logo(logo) {}
+
+    friend class Platform;
+};
+
+class Platform {
+
+    string name;
+    Logo logo;
+
+public:
+    Platform(const string& name, const string& logo) : name(name), logo(logo) {}
+
+    void changeLogo(const string& newLogo) {
+        logo.logo = newLogo;
+    }
+
+    void display() const {
+        cout << "Platform: " << name << endl;
+        cout << "Logo: " << logo.logo << endl;
+    }
+};
+
+class Server {
+
+    string type;
+    int capacity;
+
+public:
+    Server(const std::string& type, int capacity) : type(type), capacity(capacity) {}
+
+    void display() const {
+        cout << "Server Type: " << type << endl;
+        cout << "Capacity: " << capacity << "GB" << endl;
+    }
+};
+
+int main() {
+    Platform twitter("Twitter", "Blue Bird");
+    twitter.display();
+
+    cout << std::endl;
+
+    // Changing logo to Dogecoin
+    twitter.changeLogo("Dogecoin");
+    twitter.display();
+
+    return 0;
+}//end main 
 
